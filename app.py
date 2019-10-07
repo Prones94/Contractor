a765ae2316127fe597ac20191e68a2b4c54e60b8
Ian Rones <ronesp.85@gmail.com>
2019-10-07 09:15:36 -0700

    [add]homepage.html can list all types of beans

--- app.py
+++ app.py
@@ -3,18 +3,29 @@ from pymongo import MongoClient
 # from bson.objectid import ObjectId
 import os
 
-app = Flask(__name__) # sets up Flask variable
+app = Flask(__name__)  # sets up Flask variable
 
 # Tells Flask how to find database
-client = MongoClient(host=f'{host}?retryWrites=false')
-db = client.get_default_database()
-playlists = db.playlists
+# client = MongoClient(host=f'{host}?retryWrites=false')
+# db = client.get_default_database()
+# playlists = db.playlists
 
 
 @app.route('/')
 def homepage():
     '''Shows homepage'''
-    return render_template('homepage.html')
+    coffee_beans = [
+        {'name': 'Arabica', 'location': 'Latin America',
+            'smell': 'chocolatey', 'taste': 'varies'},
+        {'name': 'Robusta', 'location': 'Africa',
+         'smell': 'earthy', 'taste': 'bitter'},
+        {'name': 'Liberica', 'location': 'Philippines',
+         'smell': 'fruity', 'taste': 'smokey'},
+        {'name': 'Arabica', 'location': 'Latin America',
+         'smell': 'tart', 'taste': 'tart'}
+    ]
+    return render_template('homepage.html', coffee_beans=coffee_beans)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
