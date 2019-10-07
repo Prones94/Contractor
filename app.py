95338ee6953d3df9a9ff57032cbe2c8d6b16c73f
Ian Rones <ronesp.85@gmail.com>
2019-10-07 07:38:59 -0700

    [add] .gitignore file and env file

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from pymongo import MongoClient
 # from bson.objectid import ObjectId
 import os
 
-app = Flask(__name__)
+app = Flask(__name__) # sets up Flask variable
+
+# Tells Flask how to find database
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+playlists = db.playlists
 
 
 @app.route('/')
