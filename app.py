7143dd454ce58fa1c1d17cabf542cc53b49a5655
Ian Rones <ronesp.85@gmail.com>
2019-10-09 18:05:14 -0700

    [fix] edit-beans.html and added delete button

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ else:
 def homepage():
     '''Shows homepage'''
 
-    return render_template('index.html', coffee_beans=coffee_beans.find())
+    return render_template('index.html', coffee_beans=coffee_beans.find({}))
 
 
 @app.route('/new_beans')
@@ -61,20 +61,35 @@ def beans_submit():
     return redirect(url_for('show_list', coffee_id=coffee_id))
 
 
-@app.route('/coffee_beans/<beans_id>')
-def show_list(beans_id):
+@app.route('/coffee_beans/<coffee_id>')
+def show_list(coffee_id):
     '''Shows individual bean information'''
-    new_bean = coffee_beans.find_one({'_id': ObjectId(beans_id)})
+    new_bean = coffee_beans.find_one({'_id': ObjectId(coffee_id)})
+    # return redirect(url_for('edit_info', new_bean=new_bean))
     return render_template('show-beans.html', new_bean=new_bean)
 
 
-@app.route('/coffee_beans/{{bean._id}}/edit')
-def edit_info(beans_id):
-    '''Shows form to edit information for coffee bean information.'''
-    bean_info = coffee_beans.find_one({'_id': ObjectId(beans_id)})
-    return render_template('edit-beans.html', bean_info=bean_info)
+@app.route('/coffee_beans/<coffee_id>/edit', methods=['GET', 'POST'])
+def edit_info(coffee_id):
+    if request.method == 'POST':
+        # When the form is submitted, you can do that logic here.
+        updated_bean = {
+            'name': request.form.get('name'),
+            'location': request.form.get('location'),
+            'smell': request.form.get('smell'),
+            'taste': request.form.get('taste')
+        }
+        print(type(updated_bean['name']))
+        print(type(ObjectId(coffee_id)))
+        coffee_beans.update_one({'_id': ObjectId(coffee_id)}, {'$set': updated_bean})
 
-@app.route('/coffee_beans/<beans_id>',methods=['POST'])
+        return redirect(url_for('show_list', coffee_id=coffee_id))
+
+    '''Shows form to edit information for coffee bean information.'''
+    bean_detail = coffee_beans.find_one({'_id': ObjectId(coffee_id)})
+    return render_template('edit-beans.html', bean_detail=bean_detail)
+
+@app.route('/coffee_beans/<beans_id>', methods=['POST'])
 def update_beancart(beans_id):
     '''This will submit the updated cart'''
     new_cart = {
@@ -87,6 +102,12 @@ def update_beancart(beans_id):
         {'_id':ObjectId(beans_id)},
         {'$set': new_cart})
     return redirect(url_for('homepage', beans_id=beans_id))
+
+@app.route('/coffee_beans/<new_beans_id>/delete', method=['POST'])
+def delete_beans(new_beans_id):
+    '''Deletes one type of bean....I think'''
+    coffee_beans.delete_one({'_id': ObjectId(new_beans_id)})
+    return redirect(url_for('homepage'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
